e40e668936f19065bc13efa663c9b318147b7499
つしお <102880644+2shi0@users.noreply.github.com>
2024-06-10 19:01:09 +0900

    整理

--- kmr_m6_controller.ino
+++ kmr_m6_controller.ino
@@ -1,6 +1,6 @@
-#include "ard_ics.h"
-#include "ble_uart.h"
-#include "draw_led.h"
+#include "src/ard_ics.h"
+#include "src/ble_uart.h"
+#include "src/draw_led.h"
 
 ard_ics ics(21);
 ble_uart ble;
@@ -11,24 +11,23 @@ void setup() {
   //while (!Serial) delay(1);
 
   ble.init("kmr-m6-01");
+
   ble.create_task("ble_task");
 
   led.create_task("led_task");
-
-  led.set_mode(BLUE, LIT);
 }
 
 void loop() {
-  // 接続チェック＆LED表示
   if (!ble.get_deviceConnected()) {
     led.set_mode(BLUE, BLINK);
   } else {
     led.set_mode(GREEN, LIT);
   }
 
-
-  //ics.set_pos(6, 5000);
-  //delay(1000);
-  //ics.set_pos(6, 10000);
-  //delay(1000);
+  /*
+  ics.set_pos(6, 5000);
+  delay(1000);
+  ics.set_pos(6, 10000);
+  delay(1000);
+  */
 }
